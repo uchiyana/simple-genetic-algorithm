7862244d41f5aef1c38cb7f14b18d3f009f0e1c3
uchiyana <uchiya.na401@gmail.com>
2019-11-10 17:15:44 +0900

    Add dna class

--- ga.py
+++ ga.py
@@ -1,3 +1,30 @@
+import random
+import string
+from itertools import zip_longest
+
+
+class DNA:
+    def __init__(self, size):
+        self.genes = random.choices(
+            string.ascii_letters + string.digits + string.whitespace, k=size)
+
+    def calc_fitness(self, target):
+        self.fitness = sum([(g == t)
+                            for g, t in zip_longest(self.genes, target)])
+
+    def mutate(self, mutation_rate):
+        for i in range(len(self.genes)):
+            if(random.random() < mutation_rate):
+                self.genes[i] = random.choice(
+                    string.ascii_letters + string.digits + string.whitespace)
+
+    def crossover(self, partner):
+        midpoint = random.randrange(0, len(self.genes)-1)
+        child = DNA(len(self.genes))
+        child.genes = self.genes[:midpoint] + partner.genes[midpoint:]
+        return child
+
+
 def main():
     print("Hello")
 
